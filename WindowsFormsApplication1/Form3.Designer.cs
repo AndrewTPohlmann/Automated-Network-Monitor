2c5508cdf6399c2e59f933aa5c02e9fab0b5b317
jsfraptor26@gmail.com <jsfraptor26@gmail.com>
2015-05-16 13:45:41 -0500

    Added ping data sets for testing purposes.

--- WindowsFormsApplication1/Form3.Designer.cs
+++ WindowsFormsApplication1/Form3.Designer.cs
@@ -172,6 +172,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.CancelButton = this.button1;
             this.ClientSize = new System.Drawing.Size(1026, 684);
             this.Controls.Add(this.groupBox1);
