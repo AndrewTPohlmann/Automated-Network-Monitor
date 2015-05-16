487d2f1d3069171f7dcc5f979399f5e30281ead6
jsfraptor26@gmail.com <jsfraptor26@gmail.com>
2015-05-16 13:27:06 -0500

    Test

---
+++ WindowsFormsApplication1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+    public class RTTDataSet
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        public string name { get; set; }
+        public string configuration { get; set; }
+
+        public List<DateTime> datetimeList { get; set; }
+        public List<int> rttvalueList { get; set; }
+
+
+
+    }
+
